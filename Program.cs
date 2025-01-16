a7e4b06c7afe48cac6a5587262bd1f210df2be81
AAllred18 <161763678+AAllred18@users.noreply.github.com>
2025-01-16 12:34:34 -0700

    Add files via upload
    .cs files for Program and RollDice

---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿// Author: AJ Allred
+// Date: 1/15/2025
+// Description: This is a program that rolls 2 dice x amount of times, which is determined by the user.
+// Following the rolls, the results are displayed using astericks. This assignment demonstrates using classes in C#.
+
+using System.Collections.Generic;
+using Mission2Dice;
+
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+        //Create instances of other classes
+        RollDice rd = new RollDice();
+
+        //Creating variables used later
+        int numRolls = 0;
+        string input = "";
+        //Dictionary<int, int> rollResults = new Dictionary<int, int>();
+
+        Console.WriteLine("Welcome to the dice throwing simulator!\n");
+        Console.WriteLine("How many dice rolls would you like to simulate?\n");
+
+        //Create loop if user input is not an integer
+        while (true)
+        {
+            input = Console.ReadLine();
+
+            // Try to convert the input to an integer
+            if (int.TryParse(input, out numRolls))
+            {
+                //Console.WriteLine("You entered: " + numRolls);
+                break; // Exit the loop when an integer
+            }
+            else
+            {
+                Console.WriteLine("Invalid input. Please enter a valid number.");
+            }
+        }
+
+        //Roll dice as many times as inputted and store it in a dictionary with its value - referenced in RollDice.cs
+        //rollResults = rd.RollDiceMultipleTimes(numRolls);
+
+        //Roll dice as many times as inputted and store it in a dictionary with its value - referenced in RollDice.cs
+        int[] rollResults = rd.RollDiceMultipleTimes(numRolls);
+
+        Console.WriteLine("\nDICE ROLLING SIMULATION RESULTS");
+        Console.WriteLine("Each \"*\" represents 1% of the total number of rolls.");
+        Console.WriteLine("Total number of rolls = " + numRolls + "\n");
+
+        // Print the results of the number of rolls - referenced in RollDice.cs
+        rd.PrintResults(rollResults, numRolls);
+
+        Console.WriteLine("\nThank you for using the dice throwing simulator. Goodbye!");
+    }
+
+}
+    
---
+++ RollDice.cs
@@ -0,0 +1,108 @@
+﻿// Author: AJ Allred
+// Date: 1/15/2025
+// Description: Class for rolling dice. Contains methods that return a random number between 1 and 6, store the results in a dictionary,
+// then displays the results.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mission2Dice
+{
+    internal class RollDice
+    {
+        // Create a Random object to generate random numbers
+        private Random random = new Random();
+
+        // Roll dice numRolls amount and stores the totals in an array
+        public int[] RollDiceMultipleTimes(int numRolls)
+        {
+            int[] rolls = new int[numRolls];
+
+            for (int i = 0; i < numRolls; i++)
+            {
+                // Generate a random number between 1 and 6 and store both rolls
+                int roll1 = random.Next(1, 7);
+                int roll2 = random.Next(1, 7);
+                rolls[i] = roll1 + roll2;
+            }
+
+            return rolls;
+        }
+
+        // Calculates the frequency of each possible total (2-12)
+        public int[] CalculateFrequencies(int[] rolls)
+        {
+            int[] frequencies = new int[11]; // Array to store counts for totals 2-12
+
+            foreach (int roll in rolls)
+            {
+                frequencies[roll - 2]++; // Map roll (2-12) to position of (0-10)
+            }
+
+            return frequencies;
+        }
+
+        // Prints the results with percentages and asterisks
+        public void PrintResults(int[] rolls, int numRolls)
+        {
+            int[] frequencies = CalculateFrequencies(rolls);
+
+            for (int i = 0; i < frequencies.Length; i++)
+            {
+                int total = i + 2; // Map index back to total (2-12)
+                // numRolls is cast to a double to perform floating-point division then converted back to an integer so there are no decimals
+                int percentage = (int)((frequencies[i] / (double)numRolls) * 100);
+                string asterisks = new string('*', percentage);
+
+                Console.WriteLine($"{total}: {asterisks}");
+            }
+        }
+    }
+
+
+        // Method to simulate multiple dice rolls and store results in a dictionary
+        // Used a dictionary to store it in an easy way to display number rolled and calculate percentage
+        //public Dictionary<int, int> RollDiceMultipleTimes(int numRolls)
+        //{
+        //    // Initialize dictionary to store the count of each dice face (2-12)
+        //    Dictionary<int, int> rollResults = new Dictionary<int, int>();
+
+        //    // Initialize the dictionary all set to 0
+        //    for (int i = 2; i <= 12; i++)
+        //    {
+        //        rollResults[i] = 0; 
+        //    }
+
+        //    // Roll the dice numRolls amount
+        //    for (int i = 0; i < numRolls; i++)
+        //    {
+        //        // Generate a random number between 1 and 6 and store both rolls
+        //        int roll1 = random.Next(1, 7); 
+        //        int roll2 = random.Next(1, 7);
+        //        int rollTotal = roll1 + roll2; // Calculate the total roll
+        //        rollResults[rollTotal]++; // Increment the count of the rolled number
+        //    }
+
+        //    return rollResults; // Return the dictionary containing the roll results
+        //}
+
+        //// Method to Display roll results while using asterisks to represent 1%
+        //public void PrintResults(Dictionary<int, int> dictionary, int numRolls)
+        //{
+        //    foreach (var kvp in dictionary)
+        //    {
+        //        // numRolls is cast to a double to perform floating-point division then converted back to an integer so there are no decimals
+        //        int percentage = (int)((kvp.Value / (double)numRolls) * 100); // Calculate percentage
+        //        string asterisks = new string('*', percentage); // Create a string of asterisks
+        //        // Write results with the key paired with its number of asterisks 
+        //        Console.WriteLine($"{kvp.Key}: {asterisks}");
+
+        //        // To test and see correct percentages
+        //        //Console.WriteLine($"{kvp.Key}: {asterisks} ({percentage}%)");
+        //    }
+        //}
+    //}
+}
